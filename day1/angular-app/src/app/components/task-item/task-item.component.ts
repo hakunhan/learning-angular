463fdb6f2eb35f699fbee2bfd5a5f2d4f9b7baac
hakunhan <hakunhan@gmail.com>
2021-07-03 19:08:55 +0700

    add local app for app tracker

---
+++ day1/angular-app/src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Task} from "../../Task";
+import { faTimes} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminderTask: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggleReminder(task: Task){
+    this.onToggleReminderTask.emit(task);
+  }
+}
